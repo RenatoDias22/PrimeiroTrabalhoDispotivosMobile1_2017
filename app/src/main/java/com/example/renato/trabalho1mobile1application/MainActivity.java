8b39e3a5ca07c20853f3313c5fb1626cca4ebe56
RenatoDias22 <lrds22@hotmail.com>
2017-03-12 20:18:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/renato/trabalho1mobile1application/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.renato.trabalho1mobile1application;
+
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.media.MediaPlayer;
+import android.provider.AlarmClock;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    public Button Camera;
+    public Button Video;
+    public Button Map;
+    private ImageView imageView;
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Camera=(Button)findViewById(R.id.button_camera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        Video = (Button) findViewById(R.id.button_video);
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        Map = (Button) findViewById(R.id.button_map);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+    public void clickCamera(View v){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    public void clickVideo(View v){
+//        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+//        Uri vidUri = Uri.parse(vidAddress);
+//        videoView.setVideoURI(vidUri);
+//        videoView.start();
+//        MediaController vidControl = new MediaController(this);
+//        vidControl.setAnchorView(videoView);
+//        videoView.setMediaController(vidControl);
+//        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                mp.setLooping(true);
+//                videoView.start();
+//            }
+//        });
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse("https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4"), "video/mp4");
+        videoView.getContext().startActivity(intent);
+    }
+    public void clickMap(View v){
+        double latitude =  -3.7319;
+        double longitude = -38.5267;
+        String label = "Perto do Centro de Fortaleza-CE";
+        String uriBegin = "geo:" + latitude + "," + longitude;
+        String query = latitude + "," + longitude + "(" + label + ")";
+        String encodedQuery = Uri.encode(query);
+        String uriString = uriBegin + "?q=" + encodedQuery + "&z=16";
+        Uri uri = Uri.parse(uriString);
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void clickPhone(View v){
+//        String phone = "+558597720565";
+//        Intent callIntent = new Intent(Intent.ACTION_CALL);
+//        callIntent.setData(Uri.parse("tel:"+phone));
+//        startActivity(callIntent);
+        Intent dial = new Intent();
+        dial.setAction("android.intent.action.DIAL");
+        dial.setData(Uri.parse("tel:"));
+        startActivity(dial);
+    }
+
+    public void clickCalendario(View v){
+        long startMillis = System.currentTimeMillis();
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder, startMillis);
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+        startActivity(intent);
+    }
+
+    public void clickGoogle(View v){
+        String endereco = "http://www.google.com.br";
+        Uri uri = Uri.parse(endereco);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);;
+    }
+}
